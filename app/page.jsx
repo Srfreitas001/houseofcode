50a2430bf551359bd731867b229e96f6432ecc4d
Srfreitas001 <matheusmaximianus@gmail.com>
2024-10-24 17:46:09 -0300

    ...

--- app/page.jsx
+++ app/page.jsx
@@ -33,8 +33,8 @@ const professionals = [
     stats: [
 
       {num:20, text:"Anos de experiencia"},
-      {num:40, text:"Projetos concluídos"},
-      {num:15, text:"tecnologias geridas"},
+      {num:40, text:"Projec concluídos"},
+      {num:15, text:"tech geridas"},
       {num:100, text:"Code Commits"}
       
     ]
@@ -59,8 +59,8 @@ const professionals = [
     stats: [
 
       {num:10, text:"Anos de experiencia"},
-      {num:30, text:"Projetos concluídos"},
-      {num:10, text:"tecnologias geridas"},
+      {num:30, text:"Projec concluídos"},
+      {num:10, text:"tech geridas"},
       {num:50, text:"Code Commits"}
       
     ]
@@ -85,8 +85,8 @@ const professionals = [
     stats: [
 
       {num:1, text:"Anos de experiencia"},
-      {num:6, text:"Projetos concluídos"},
-      {num:5, text:"tecnologias geridas"},
+      {num:6, text:"Projec concluídos"},
+      {num:5, text:"tech geridas"},
       {num:4, text:"Code Commits"}
 
     ]
