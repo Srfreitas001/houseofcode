f6322bacaba82718cb7419f6c8e4e86ef4079086
Srfreitas001 <matheusmaximianus@gmail.com>
2024-10-24 17:30:53 -0300

    ...

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ const professionals = [
     name: "CEO Fullstack Developer",
     title: "Gleison",
     description: `Sou Gleison Freitas, CEO e desenvolvedor Full Stack da HouseOfCode. Com expertise em front-end e back-end, lidero projetos com inovação e eficiência. Combino habilidades técnicas
-     e visão estratégica para entregar soluções de alta qualidade.`,
+     e visão estratégica para entregar soluções de alta qualidade. kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkllllllllllllllll`,
     photo: "/assets/GleisonCEO-01.png",  //{/*picture url*/}
     cv: "/assets/Gleisonfreitas-curriculo.png",
     dwnl:"Gleison Freitas - CEO Fullstack Developer",
@@ -42,8 +42,8 @@ const professionals = [
   {
     name: "Fullstack Developer",
     title: "Reynaldo",
-    description: `Sou Reynaldo de Souza, desenvolvedor Full Stack da HouseOfCode, com foco em criar soluções eficientes e escaláveis em front-end e back-end. Minhas habilidades técnicas são voltadas a resultados
-     de qualidade e inovação para o sucesso dos projetos.`,
+    description: `Sou Reynaldo de Souza, desenvolvedor Full Stack da HouseOfCode, com foco em criar soluções eficientes e escaláveis em front-end e back-end. Minhas habilidades técnicas
+     são voltadas a resultados de qualidade e inovação para o sucesso dos projetos. kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk`,
     photo: "/assets/ReynaldoDevfullstack-01.png", // {/*picture url*/}
     cv: "/assets/Reynaldodesouza-curriculo.png",
     dwnl: "Reynaldo de Souza - Fullstack Developer",
