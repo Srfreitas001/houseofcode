c1fda9f6e5479901137eac9200ad040aba75ac51
Srfreitas001 <matheusmaximianus@gmail.com>
2024-11-10 18:12:03 -0300

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = {};
 
-    eslint: {
-
-        ignoreDuringBuilds: true,
-    },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
